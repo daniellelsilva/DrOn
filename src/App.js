c57bf6f02898069c7e853a3e7401a1281d85a233
daniellelsilva <danielle.luisasilva@gmail.com>
2023-01-29 15:31:11 -0300

    WIP initial structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import Agenda from './pages/agenda/Agenda';
+import AppView from './pages/app/AppView';
+import Carrer from './pages/career/Carrer';
+import Events from './pages/events/Events';
+import FiveSteps from './pages/fiveSteps/FiveSteps';
+import Footer from './pages/footer/Footer';
+import Intro from './pages/intro/Intro';
 
 function App() {
   return (
     <div className="App">
-      
+      <Intro/>
+      <FiveSteps/>
+      <Carrer/>
+      <Events/>
+      <Agenda/>
+      <AppView/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/pages/app/AppView.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AppView() {
+  return (
+    <div>AppView</div>
+  )
+}
