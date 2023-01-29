49bf0c8930fe57c034147ff036920e40651c4570
daniellelsilva <danielle.luisasilva@gmail.com>
2023-01-29 20:10:59 -0300

    add icons

--- src/pages/fiveSteps/FiveSteps.jsx
+++ src/pages/fiveSteps/FiveSteps.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React from 'react';
+import { Icon } from '@iconify/react';
+
+import './FiveSteps.scss';
 
 export default function FiveSteps() {
   return (
-    <div>FiveSteps</div>
+    <main className='steps'>
+      <section className='steps-title'>
+        <p>Mentoria em Health Business</p>
+        <p>5 passos</p>
+      </section>
+
+      <section className='steps-cards-section'>
+        <div className='steps-cards'>
+          <div className='steps-card steps-card-1'>
+            <div className='steps-card-icon'>
+              <Icon icon="mdi:map-marker" />
+            </div>
+          </div>
+
+          <div className='steps-card steps-card-2'>
+            <div className='steps-card-icon'>
+              <Icon icon="ri:treasure-map-fill" />
+            </div>
+          </div>
+
+          <div className='steps-card steps-card-3'>
+            <div className='steps-card-icon'>
+              <Icon icon="mdi:account-multiple" />
+            </div>
+          </div>
+
+          <div className='steps-card steps-card-4'>
+            <div className='steps-card-icon'>
+              <Icon icon="material-symbols:potted-plant" />
+            </div>
+          </div>
+
+          <div className='steps-card steps-card-5'>
+            <div className='steps-card-icon'>
+              <Icon icon="material-symbols:star-rate-rounded" />
+            </div>  
+          </div>
+
+        </div>
+      </section>
+
+      <section className='steps-info'>
+        <div className='steps-info-text'>
+          <p>Converse com a gente!</p>
+          <p>Saiba mais sobre nossa mentoria, projetos complementares, convites para eventos, dúvidas gerais e tudo mais que você precisar! Basta escolher o meio de contato!</p>
+
+          <div>botão whats</div>
+          <div>botão email</div>
+        </div>
+      </section>
+    </main>
   )
 }
