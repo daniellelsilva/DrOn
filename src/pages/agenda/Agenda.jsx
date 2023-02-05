709f367925c10dc98f8f11fc2c26291d437f1b54
daniellelsilva <danielle.luisasilva@gmail.com>
2023-02-05 20:16:03 -0300

    add agenda

--- src/pages/agenda/Agenda.jsx
+++ src/pages/agenda/Agenda.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import AgendaObject from './AgendaObject'
 
 export default function Agenda() {
   return (
-    <div>Agenda</div>
+    <main>
+      <div>
+        <p>Agenda</p>
+        <p>Próximos eventos</p>
+      </div>
+
+      <section>
+        {AgendaObject.map((agenda) => (
+          <div>
+            <div>
+              <p>{agenda.day}</p>
+              <p>{agenda.month}</p>
+              <p>{agenda.year}</p>
+            </div>  
+          
+            <div>
+              <div>
+                <p>{agenda.event}</p>
+                <p>{agenda.eventName}</p>
+              </div>
+            
+              <p>{agenda.info}</p>
+            </div>
+          </div>
+        ))}
+      </section>
+    </main>
   )
 }
---
+++ src/pages/agenda/AgendaObject.jsx
@@ -0,0 +1,36 @@
+const AgendaObject = [
+  {
+    day:'',
+    month: 'MAR',
+    year:'2023',
+    event: 'sbot rj',
+    eventName: 'Dr.On Day',
+    info: 'Dia todo dedicado a conteúdos agregadores para médicos.'
+  },
+  {
+    day:'',
+    month: 'MAI',
+    year: '2023',
+    event: 'CBRH',
+    eventName: 'Dr.On Day',
+    info: 'Dia todo dedicado a conteúdos agregadores para médicos.'
+  },
+  {
+    day:'15',
+    month: 'mai',
+    year: '2023',
+    event: 'MANDIC - SÃO LEOPOLDO RS',
+    eventName: 'INOVAÇÃO E ESTRATÉGIA',
+    info: 'Inovação e estratégia com os alunos do Grupo São Leopoldo Mandic'
+  },
+  {
+    day:'',
+    month: 'AGO',
+    year: '2023',
+    event: 'MARKETING MÉDICO BH',
+    eventName: 'DR.ON DAY BH',
+    info: 'Palestra marketing médico e estratégias na saúde BH'
+  },
+]
+
+export default AgendaObject;
