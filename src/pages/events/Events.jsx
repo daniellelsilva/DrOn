7f42f900c5a0cacd814d261a6e2efdf0c9ec9926
daniellelsilva <danielle.luisasilva@gmail.com>
2023-02-05 18:15:01 -0300

    add carousel

--- src/pages/events/Events.jsx
+++ src/pages/events/Events.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Carousel } from '@mantine/carousel';
+
+import './Events.scss';
 
 export default function Events() {
   return (
-    <div>Events</div>
+    <main className='events'>
+      <Carousel
+        slideSize="70%"
+        height={230}
+        slideGap="md"
+        withIndicators
+        loop
+      >
+      <Carousel.Slide>1</Carousel.Slide>
+      <Carousel.Slide>2</Carousel.Slide>
+      <Carousel.Slide>3</Carousel.Slide>
+      {/* ...other slides */}
+    </Carousel>
+    </main>
   )
 }
