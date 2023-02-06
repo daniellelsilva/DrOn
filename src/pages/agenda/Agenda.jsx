62651068f8b282c98a72bf5fd8b8d61a8d27c4e7
daniellelsilva <danielle.luisasilva@gmail.com>
2023-02-06 00:32:08 -0300

    add agenda

--- src/pages/agenda/Agenda.jsx
+++ src/pages/agenda/Agenda.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import AgendaObject from './AgendaObject'
+import React from 'react';
+import AgendaObject from './AgendaObject';
+
+import './Agenda.scss';
 
 export default function Agenda() {
   return (
-    <main>
-      <div>
+    <main className='agenda'>
+      <div className='agenda-title'>
         <p>Agenda</p>
         <p>Próximos eventos</p>
       </div>
 
-      <section>
+      <section className='agenda-events'>
         {AgendaObject.map((agenda) => (
-          <div>
-            <div>
+          <div className='agenda-event'>
+            <div className='agenda-event-date'>
               <p>{agenda.day}</p>
               <p>{agenda.month}</p>
               <p>{agenda.year}</p>
             </div>  
           
-            <div>
-              <div>
+            <div className='agenda-event-infos'>
+              <div className='agenda-event-name'>
                 <p>{agenda.event}</p>
                 <p>{agenda.eventName}</p>
               </div>
             
-              <p>{agenda.info}</p>
+              <p className='agenda-event-info'>{agenda.info}</p>
             </div>
           </div>
         ))}
