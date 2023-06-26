d18352ad1866611ec3e44b0fe0013bce490d34e6
Danielle Silva <danielle.luisasilva@gmail.com>
2023-06-26 20:28:10 -0300

    update agenda

--- src/pages/agenda/AgendaObject.jsx
+++ src/pages/agenda/AgendaObject.jsx
@@ -1,27 +1,27 @@
 const AgendaObject = [
   {
     day:'',
-    month: 'MAI',
+    month: 'JUN',
     year: '2023',
-    event: 'CBRH',
-    eventName: 'Dr.On Day',
-    info: 'Dia todo dedicado a conteúdos agregadores para médicos.'
-  },
-  {
-    day:'15',
-    month: 'mai',
-    year: '2023',
-    event: 'MANDIC - SÃO LEOPOLDO RS',
-    eventName: 'INOVAÇÃO E ESTRATÉGIA',
-    info: 'Inovação e estratégia com os alunos do Grupo São Leopoldo Mandic.'
+    event: 'São Paulo SP',
+    eventName: 'Projeto Consultório Gold',
+    info: 'Marketing médico | Dr.On®Day | Mesa redonda sobre Health Business.'
   },
   {
     day:'',
     month: 'SET',
     year: '2023',
-    event: 'BELO HORIZONTE MG',
-    eventName: 'TRÊS PALESTRAS IMPERDÍVEIS',
+    event: 'Belo Horizonte MG',
+    eventName: 'Três palestras imperdíveis',
     info: 'Marketing médico | Dr.On®Day | Mesa redonda sobre Health Business.'
+  },
+  {
+    day:'',
+    month: 'OUT',
+    year: '2023',
+    event: 'MANDIC - SÃO LEOPOLDO RS',
+    eventName: 'Inovação e estratégia',
+    info: 'Inovação e estratégia com os alunos do Grupo São Leopoldo Mandic'
   },
 ]
 
